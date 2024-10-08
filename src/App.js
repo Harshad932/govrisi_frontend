b441763182aa8596f90186caa0bfea41bc813e77
himanshuraghorteatwork <himanshuraghorteatwork@gmail.com>
2024-10-08 16:13:00 +0530

    lastpage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import './assets/styles/ResearchForm.css';
+import './assets/styles/Homepage.css';
+import './assets/styles/SignInForm.css';
+import './assets/styles/ReasearchSearchWindow.css';
 
-import './App.css';
 import ResearchForm from './components/ResearchForm';
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
+import SignInForm from './components/SignInForm';
+import ReasearchSearchWindow from './components/ReasearchSearchWindow';
+
 
 function App() {
   return (
     <>
     
-       <HomePage/>
+      {/* <HomePage/>
        <ResearchForm/>
+       <SignInForm/>
+       <Footer/>
+        <ReasearchSearchWindow/>
+       */}
+      
+       <ReasearchSearchWindow/>
+       
 
     
     </>
